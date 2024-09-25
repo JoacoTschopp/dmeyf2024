9b8c336277226b599a864f6440392e226fb346e1
joacotschopp <joaquintschopp@gmail.com>
2024-09-25 17:04:14 -0300

    CT7

--- 00TP-Julia-Tschopp.jl
+++ 00TP-Julia-Tschopp.jl
@@ -1,20 +1,17 @@
 using CSV, DataFrames
 using BenchmarkTools
-using GoogleCloud
+
 
 periodo_anterior(x::Integer) = x % 100 > 1 ? x - 1 : 12 + (div(x, 100) - 1) * 100
 
 ###Apartado apra ejecucion en Goggle Cloud y Bukets
 #df = CSV.read("gs://joaco333/datasets/competencia_01_crudo.csv", DataFrame)
 
-# Lee el archivo desde GCS
-bucket = GoogleCloud.get_bucket("joaco333")
-file = GoogleCloud.get_file(bucket, "datasets/competencia_01_crudo.csv")
-data = GoogleCloud.read_file(file)
+ruta = "~/datasets/competencia_01_crudo.csv"
+
 
 # Lee el archivo como un DataFrame
-using CSV
-df = CSV.read(data, DataFrame)
+df = CSV.read(ruta, DataFrame)
 
 
 
@@ -46,8 +43,4 @@ for i in 1:last
 end
 
 #CSV.write( "G:/Mi unidad/01-Maestria Ciencia de Datos/DMEyF/TPs/dmeyf-2024/datasets/competencia_01_julia.csv", df )
-
-###Apartado apra ejecucion en Goggle Cloud y Bukets
-#CSV.write("gs://joaco333/datasets/competencia_01_ct.csv", df)
-# Sube el archivo a GCS
-GoogleCloud.upload_file(gcs, "joaco333", "datasets/competencia_01_ct.csv", CSV.write(df))
+CSV.write("~/datasets/competencia_01_ct.csv", df)
