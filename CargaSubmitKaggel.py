a6437ad003945c660f2c83eed1f3b30a3df32593
JoacoTschopp <joaquintschopp@gmail.com>
2024-10-09 08:25:08 -0300

    Submit Ordenados

--- CargaSubmitKaggel.py
+++ CargaSubmitKaggel.py
@@ -6,11 +6,11 @@ os.environ['KAGGLE_CONFIG_DIR'] = '/home/joaquintschopp/buckets/b1'
 # Configura la ID de la competencia y la lista de archivos con sus descripciones
 competition = 'dm-ey-f-2024-primera'
 scores_dir = '/home/joaquintschopp/buckets/b1/scores'
-experiment_name = 'KA7250SA'
+experiment_name = 'KA7250SA_0'
 
 files_dir = '/home/joaquintschopp/buckets/b1/exp/' + experiment_name
 
-submission_description = 'DESCIPCION: Experimento 3 SA- Sin atributos entreno solo con 202104'
+submission_description = 'DESCIPCION: Experimento 5 SA-0 Sin atributos solo lags y delta, entreno solo con 202104'
 
 # Inicializar la API usando las credenciales de kaggle.json
 api = KaggleApi()
@@ -19,12 +19,8 @@ api.authenticate()
 files = os.listdir(files_dir)
 files = [f for f in files if f.endswith('.csv')]
 
-# ordenar files por el numero antes de .csv
-files = sorted(files, key=lambda x: int(x.split('.')[0].split('_')[1]))
-
-# Lista de archivos a subir
 submissions = [{'file': f'{files_dir}/{f}',
-                'description': f'{submission_description}'} for f in files]
+                'description': f'{submission_description}'} for f in sorted(files, key=lambda x: int(x.split('.')[0].split('_')[1]))]
 
 # Subir los archivos a la competencia
 for submission in submissions:
