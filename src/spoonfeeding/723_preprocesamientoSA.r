285bdd4162f397051a74218169d66ab44d314308
joacotschopp <joaquintschopp@gmail.com>
2024-10-13 16:29:02 -0300

    13.4

--- src/spoonfeeding/723_preprocesamientoSA.r
+++ src/spoonfeeding/723_preprocesamientoSA.r
@@ -32,7 +32,7 @@ PARAM$input$dataset <- "./datasets/competencia_01_ct.csv"
 PARAM$semilla_azar <- 214363 # Aqui poner su  primer  semilla
 
 
-PARAM$driftingcorreccion <- "ninguno"#"estandarizar"
+PARAM$driftingcorreccion <- "estandarizar"#"ninguno"#
 PARAM$clase_minoritaria <- c("BAJA+1","BAJA+2")
 
 # los meses en los que vamos a entrenar
@@ -282,7 +282,7 @@ setwd(paste0("./exp/", PARAM$experimento, "/"))
 # ordeno dataset
 setorder(dataset, numero_de_cliente, foto_mes)
 # corrijo usando el metido MachineLearning
-Corregir_Rotas(dataset, "EstadisticaClasica")#"MachineLearning"
+Corregir_Rotas(dataset, "MachineLearning")#"EstadisticaClasica"
 
 # Eliminar columnas PROPUESTAS POR MATERIA ""CONCEPT DRIFTING""
 #"ccajas_depositos" todos ceros para 202105/06 no hay forma de arreglarlo
@@ -293,7 +293,9 @@ dataset <- dataset[, -c("cprestamos_personales", "mprestamos_personales", "ccaja
 #Estas son cantidades no es adecuado a mi entender eliminar. solo son influyentes.
 #"ccomisiones_otras", "cextraccion_autoservicio",
 #SACO EL "cpayroll_trx" PORQUE ES EL UNICO QUE SOBRESALE LUEGO DE ARREGLAR EL DRIFT EN LAS MONETARIAS
-dataset <- dataset[, -c("cpayroll_trx")]
+dataset <- dataset[, -c("cpayroll_trx")]#, "mpayroll", "Visa_mlimitecompra", "Master_mfinanciacion_limite", 
+                                    #"mcomisiones_mantenimiento", "Visa_mfinanciacion_limite", 
+                                    #"Master_mlimitecompra")]
 
 
 
@@ -316,12 +318,12 @@ campos_monetarios <- campos_monetarios[campos_monetarios %like%
   "^(m|Visa_m|Master_m|vm_m)"]
 
 ##CAMPOS ENCONTRADOS CON DRIFTING
-#campos_monetarios <- as.character(c("mpayroll", "Visa_mlimitecompra", "Master_mfinanciacion_limite", 
-#                                    "mcomisiones_mantenimiento", "Visa_mfinanciacion_limite", 
-#                                    "Master_mlimitecompra", "Visa_msaldodolares", "Visa_mconsumosdolares", 
-#                                    "mcaja_ahorro_dolares", "mtransferencias_recibidas", 
-#                                    "mtarjeta_visa_consumo", "mpasivos_margen", "mcuentas_saldo", 
-#                                    "mextraccion_autoservicio", "mactivos_margen", "mrentabilidad_annual"))
+campos_monetarios <- as.character(c("mpayroll", "Visa_mlimitecompra", "Master_mfinanciacion_limite", 
+                                    "mcomisiones_mantenimiento", "Visa_mfinanciacion_limite", 
+                                    "Master_mlimitecompra", "Visa_msaldodolares", "Visa_mconsumosdolares", 
+                                    "mcaja_ahorro_dolares", "mtransferencias_recibidas", 
+                                    "mtarjeta_visa_consumo", "mpasivos_margen", "mcuentas_saldo", 
+                                    "mextraccion_autoservicio", "mactivos_margen", "mrentabilidad_annual"))
 
 
 switch(PARAM$driftingcorreccion,
@@ -357,6 +359,10 @@ dataset[
 #  perdi el link a la tesis, NO es de mi autoria
 
 dataset[, mpayroll_sobre_edad := mpayroll / cliente_edad]
+
+#dataset <- dataset[, -c("mpayroll")]
+
+
 
 
 # Por supuesto, usted puede COMENTARIAR todo lo que desee
@@ -505,7 +511,7 @@ dataset[, azar := NULL ]
 
 # Grabo el dataset
 fwrite( dataset,
-  file = "dataset13.3.csv.gz",
+  file = "dataset13.4.csv.gz",
   sep = "\t"
 )
 
