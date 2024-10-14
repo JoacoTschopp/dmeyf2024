69f374c0aee5caa01c1daf7375f909b1cdd81168
joacotschopp <joaquintschopp@gmail.com>
2024-10-13 22:50:16 -0300

    Con Test en Kaggle

--- src/spoonfeeding/745_lightgbm_final.r
+++ src/spoonfeeding/745_lightgbm_final.r
@@ -29,9 +29,9 @@ options(error = function() {
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
 PARAM$experimento_data <- "PP7230"#PP7430
-PARAM$experimento_bayesiana <- "HT744013.6"
+PARAM$experimento_bayesiana <- "HT7440Kaggle"
 
-PARAM$experimento <- "KA745013.6"
+PARAM$experimento <- "KA7450Kaggle"
 
 PARAM$semilla_azar <- 214363 # Aqui poner su  primer  semilla
 PARAM$semillas_cantidad <- 3
@@ -73,7 +73,7 @@ ksemillas <- sample(primos)[seq(PARAM$semillas_cantidad)]
 tb_BO_log <- fread(paste0(PARAM$experimento_bayesiana,"/BO_log.txt"))
 
 # cargo el dataset donde voy a entrenar el modelo
-dataset <- fread(paste0(PARAM$experimento_data,"/dataset13.6.csv.gz"))
+dataset <- fread(paste0(PARAM$experimento_data,"/datasetKaggle.csv.gz"))
 
 # En un mundo prolijo, estas variables se eliminan
 #  durante la creacion del dataset
