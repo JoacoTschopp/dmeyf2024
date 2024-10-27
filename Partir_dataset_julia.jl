a938c0274d616ac8c974e78d8101ddc258bc5b09
joacotschopp <joaquintschopp@gmail.com>
2024-10-27 20:33:11 -0300

    J27.1

--- Partir_dataset_julia.jl
+++ Partir_dataset_julia.jl
@@ -3,22 +3,25 @@ using Dagger, CSV, DataFrames
 # Definir el dataset
 @info "Comienza la carga del dataset"
 file_path = "/home/joaquintschopp/buckets/b1/datasets/competencia_02_ct.csv.gz"
-dataset = CSV.File(file_path) |> DataFrame
+dataset = CSV.File(file_path; buffer_in_memory=true) |> DataFrame
 @info "Carga del dataset completada"
 
 # Definir los meses para el filtro
 training_months = [202108, 202107, 202106, 202105, 202104, 202103, 202102, 202101]
 
-# Crear un proceso de filtro paralelo usando Dagger
+# Crear una tarea para filtrar los datos
 @info "Filtrando dataset"
-filtered_dataset = @spawn dataset[dataset.foto_mes.∈training_months, :]
 
-# Esperar a que se complete la tarea y recoger los datos
-filtered_dataset = collect(filtered_dataset)
-@info "Filtrado completado"
+# Usar Dagger para crear un flujo de trabajo
+filtered_data = Dagger.@dagger dataset[dataset.foto_mes.∈training_months, :]
+
+# Ejecutar el trabajo de Dagger y recoger los resultados
+@info "Ejecutando el filtro"
+filtered_result = Dagger.collect(filtered_data)
 
 # Guardar el dataset filtrado
 output_path = "/home/joaquintschopp/buckets/b1/datasets/competencia_julia_ct.csv.gz"
 @info "Guardando el dataset filtrado"
-CSV.write(output_path, filtered_dataset, compress=true)
+CSV.write(output_path, filtered_result, compress=true)
 @info "Dataset guardado con éxito en $(output_path)"
+
