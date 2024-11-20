f88755efe5593ebf845a3044fb37d69552d6456e
JoacoTschopp <joaquintschopp@gmail.com>
2024-11-20 13:08:22 -0300

    Exp_stacking02_s1

--- src/workflows/916_workflow_base_f202106_Serv2.r
+++ src/workflows/916_workflow_base_f202106_Serv2.r
@@ -12,8 +12,8 @@ if( !exists("envg") ) envg <- env()  # global environment
 
 envg$EXPENV <- list()
 envg$EXPENV$bucket_dir <- "~/buckets/b1"
-envg$EXPENV$exp_dir <- "~/buckets/b1/expw/"
-envg$EXPENV$wf_dir <- "~/buckets/b1/flow/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/expw_stacking02_nivel2/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow_stacking02_nivel2/"
 envg$EXPENV$repo_dir <- "~/dmeyf2024/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$messenger <- "~/install/zulip_enviar.sh"
