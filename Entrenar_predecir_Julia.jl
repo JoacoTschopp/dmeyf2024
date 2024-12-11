b15fbabcd4d3f2b936ea9808abf106261ec0a7b8
JoacoTschopp <joaquintschopp@gmail.com>
2024-12-11 09:52:55 -0300

    4

--- Entrenar_predecir_Julia.jl
+++ Entrenar_predecir_Julia.jl
@@ -63,18 +63,15 @@ validation_testing_bo = filter(row -> row.foto_mes in validation_bo || row.foto_
 training_bo_dataset = filter(row -> row.foto_mes in training_bo, dataset_bo)
 
 # Separar clases mayoritaria y minoritaria
-majority_class_rows = filter(row -> row.clase_ternaria != clase_minoritaria, training_bo_dataset)
+majority_class_rows = collect(eachrow(filter(row -> row.clase_ternaria != clase_minoritaria, training_bo_dataset)))
 minority_class_rows = filter(row -> row.clase_ternaria == clase_minoritaria, training_bo_dataset)
 
 # Submuestrear aleatoriamente la clase mayoritaria
 sample_size_bo = min(undersampling_bo, nrow(majority_class_rows))
 selected_majority = shuffle(majority_class_rows)[1:sample_size_bo]
 
-# Combinar clase minoritaria y submuestreo de clase mayoritaria
-training_bo_dataset = vcat(minority_class_rows, selected_majority, cols=:union)
-
-# Combinar todo el dataset
-dataset_bo = vcat(training_bo_dataset, validation_testing_bo, cols=:union)
+# Combinar todo el dataset en una sola llamada
+dataset_bo = vcat(minority_class_rows, selected_majority, validation_testing_bo, cols=:union)
 
 # Guardar el DataFrame combinado en un archivo CSV
 output_file_path = "/home/joaquintschopp/buckets/b1/expe_julia/dataset_bo.csv"
